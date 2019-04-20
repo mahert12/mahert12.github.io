c9e7528e23dfee869e71d7f90c58a102fcc1d82b
Tim Maher <mahert12.tmaher@gmail.com>
2019-04-19 23:17:49 -0400

    Lots of CSS changes.  Using css grid and div tags for content organization.  Removed nav links, planning to add those back in soon.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 $(function () {
   count = 0;
-  // TODO: add more to this array
-  wordsArray = [ "programming", "snowboarding", "video games", "board games"];
+  prev1 = 0;
+  prev2 = 0;
+  wordsArray = [
+    "art",
+    "board games",
+    "cats",
+    "computers",
+    "design",
+    "dogs",
+    "music",
+    "programming",
+    "reading books",
+    "snowboarding",
+    "video games",
+  ];
   setInterval(
     function () {
       $("#love").fadeOut(400, function () {
-        $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+        $(this).text(wordsArray[count]).fadeIn(400);
       });
-      count++;
+      prev2 = prev1;
+      prev1 = count;
+      while (count == prev1 || count == prev2) {
+        count = Math.round(Math.random() * wordsArray.length);
+      }
+      console.log("count: " + count + " prev1: " + prev1 + " prev2: " + prev2)
     },
     4000
   );
